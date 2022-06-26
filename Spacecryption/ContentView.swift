bbfcbfe2d917043be23db62e8daca77b50dc63a6
Andrea Masturzo <92364690+AndreaMasturzo@users.noreply.github.com>
2022-06-26 05:54:57 +0200

    Picker and related functionalities

--- Spacecryption/ContentView.swift
+++ Spacecryption/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
     @State private var showScanner = false
     @State private var isRecognizing = false
     @State private var recognizedString = ""
-    
+    @State private var decryptedString = ""
+    @State private var key = ""
+    @State private var neededKey = false
+    var encryptions = ["Base64", "Reverse", "Caesar", "Skip", "AES-128"]
+    @State private var selectedEncryption = "Select"
     var decipher = Dechiper()
     
     var body: some View {
@@ -33,95 +37,55 @@ struct ContentView: View {
                         Image(systemName: "doc.text.viewfinder")
                             .renderingMode(.template)
                             .foregroundColor(.white)
-//
-//                                Text("Scan")
-//                                    .foregroundColor(.white)
                     }
                     .padding(.horizontal, 16)
-//                        .frame(height: 56)
                     .frame(width: 50, height: 50)
                     .background(Color(UIColor.systemIndigo))
                     .cornerRadius(100)
-                    
                 })
-                
-            }.padding(25)
-         
+            }
+            .padding(25)
+            
             ZStack(alignment: .bottom) {
-//                                List(recognizedContent.items, id: \.id) { textItem in
-//                                    NavigationLink(destination: TextPreviewView(text: textItem.text)) {
-//                                        Text(String(textItem.text.prefix(50)).appending("..."))
-//                                    }
-//                                }
-//
-                
                 if isRecognizing {
                     ProgressView()
                         .progressViewStyle(CircularProgressViewStyle(tint: Color(UIColor.systemIndigo)))
                         .padding(.bottom, 20)
                 }
                 VStack{
-                    ZStack{
-//                        Rectangle()
-//                            .frame(minWidth: 0, maxWidth: 350, minHeight: 0, maxHeight: 200)
-//                            .foregroundColor(.yellow)
-//                            .border(Color(.blue))
-//                            .padding()
-//                        HStack{
-//
-//                            Button(action: {
-//                                print("Text Inputfcdcdcdfc")
-//                            }){ Text("Text Input")
-//                                    .frame(minWidth: 0, maxWidth: 160, minHeight: 0, maxHeight: 150)
-//                                    .border(Color(.blue))
-//                            }
-//
-//                            Button(action: {
-//                                print("Text Inputfcdcdcdfc")
-//                            }){ Text("Camera Input")
-//                                    .frame(minWidth: 0, maxWidth: 160, minHeight: 0, maxHeight: 150)
-//                                    .border(Color(.blue))
-//                            }
-//                        }
-                        
+                    ScanTextField("Input text...", text: $recognizedString)
+                        .padding(8)
+                        .frame(height: 30)
+                        .frame(maxWidth: 300)
+                        .background(.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .padding()
+                    
+                    Picker("Decryption", selection: $selectedEncryption) {
+                        ForEach(encryptions, id: \.self) {
+                            Text($0)
+                        }
                     }
-                    ZStack{
-                        ScanTextField("Input text...", text: $recognizedString)
-                            .padding(8)
-                            .frame(height: 30)
-                            .frame(maxWidth: 300)
-                            .background(.gray.opacity(0.2))
-                            .cornerRadius(10)
-                            .padding()
-//                        TextField("Eecrypted Text", text: Value)
-//                            .frame(minWidth: 0, maxWidth: 350, minHeight: 0, maxHeight: 200)
-//                            .border(Color(.blue))
-//                            .padding()
-//                        HStack{
-//                            Spacer()
-//
-//                        }
-                        
+                    .pickerStyle(.menu)
+                    .onTapGesture {
+                        UIApplication.shared.endEditing()
                     }
                     
-//                    Spacer()
-                    TextField("Key", text: .constant(""))
-                        .frame(minWidth: 0, maxWidth: 350, minHeight: 0, maxHeight: 70)
-                        .border(Color(.blue))
-                        .padding()
-//                    HStack{
-//                        Label("Key:", systemImage: "key")
-//                        Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
-//                            /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
-//                            /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
-//                        }
-//                        Spacer()
-//                    }.padding(25)
+                    if selectedEncryption == "Caesar" || selectedEncryption == "Skip" || selectedEncryption == "AES-128" {
+                        
+                        TextField(selectedEncryption == "AES-128" ? "Key" : (selectedEncryption == "Caesar" ? "Shift" : "SkipSize"), text: $key)
+                            .frame(minWidth: 0, maxWidth: 350, minHeight: 0, maxHeight: 70)
+                            .border(Color(.blue))
+                            .padding()
+                            .keyboardType(selectedEncryption == "Caesar" || selectedEncryption == "Skip" ? .numberPad : .default)
+                    }
+                    
                     
                     TextEditor(text: .constant("Encrypted Text"))
                         .frame(minWidth: 0, maxWidth: 350, minHeight: 0, maxHeight: 200)
                         .border(Color(.blue))
                         .padding()
+                       
                     
                     Button(action: {
                         print("Azione")
@@ -130,72 +94,26 @@ struct ContentView: View {
                             Image(systemName: "lock.open")
                                 .renderingMode(.template)
                                 .foregroundColor(.white)
-
-                                Text("Decrypt")
-                                    .foregroundColor(.white)
+                            
+                            Text("Decrypt")
+                                .foregroundColor(.white)
                         }
                         .padding(.horizontal, 16)
-//                        .frame(height: 56)
+                        //                        .frame(height: 56)
                         .frame(width: 200, height: 60)
                         .background(Color(UIColor.systemIndigo))
                         .cornerRadius(100)
                     })
-                    
-                    
-//                    Text("TEST")
-//                        .foregroundColor(.black)
-//                    Text("TEST2")
-//                        .foregroundColor(.black)
-//                    Text("TEST3")
-//                        .foregroundColor(.black)
                 }
             }
-//            .navigationTitle("Spacecryption")
-//            .navigationBarItems(trailing: Button(action: {
-//                guard !isRecognizing else { return }
-//                showScanner = true
-//            }, label: {
-//                HStack {
-//                    Image(systemName: "doc.text.viewfinder")
-//                        .renderingMode(.template)
-//                        .foregroundColor(.white)
-//
-//                    Text("Scan")
-//                        .foregroundColor(.white)
-//                }
-//                .padding(.horizontal, 16)
-//                .frame(height: 36)
-//                .background(Color(UIColor.systemIndigo))
-//                .cornerRadius(18)
-//            }))
-            
         }
-        .sheet(isPresented: $showScanner, content: {
-            ScannerView { result in
-                switch result {
-                case .success(let scannedImages):
-                    isRecognizing = true
-                    
-                    TextRecognition(scannedImages: scannedImages,
-                                    recognizedContent: recognizedContent) {
-                        // Text recognition is finished, hide the progress indicator.
-                        isRecognizing = false
-                    }
-                    .recognizeText()
-                    
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-                
-                showScanner = false
-                
-            } didCancelScanning: {
-                // Dismiss the scanner controller and the sheet.
-                showScanner = false
-            }
-        })
     }
-    
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
